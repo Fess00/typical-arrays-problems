084e21984cac7411fda5303ce6af3d72f9c5176b
Fess00 <ss_s0l0@mail.ru>
2021-03-10 22:57:59 +0300

    min, max, avg - completed

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
 
 exports.min = function min (array) {
-    let minVal = 1000;
-    for (let i = 0; i < array.length; i++) {
-        if (minVal > array[i]) {
-            minVal = array[i];
+    if (!array || array == 0) {
+        return 0;
+    } else {
+        let minVal = 1000;
+        for (let i = 0; i < array.length; i++) {
+            if (minVal > array[i]) {
+                minVal = array[i];
+            }
         }
+        return minVal;
     }
-    return minVal;
 }
 
 exports.max = function max (array) {
-    let maxVal = -1000;
-    for (let i = 0; i < array.length; i++) {
-        if (maxVal < array[i]) {
-            maxVal = array[i];
+    if (!array || array == 0) {
+        return 0;
+    } else {
+        let maxVal = -1000;
+        for (let i = 0; i < array.length; i++) {
+            if (maxVal < array[i]) {
+                maxVal = array[i];
+            }
         }
+        return maxVal;
     }
-    return maxVal;
 }
 
 exports.avg = function avg (array) {
-    let count = 0;
-    for (let i = 0; i < array.length; i++) {
-        count += array[i];
+    if (!array || array == 0) {
+        return 0;
+    } else {
+        let count = 0;
+        for (let i = 0; i < array.length; i++) {
+            count += array[i];
+        }
+        count /= array.length;
+        return count;
     }
-    count /= array.length;
-    return count;
 }

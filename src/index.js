e6ff131ba7582161fa06e30afcb9fdcaf342f3e6
Fess00 <ss_s0l0@mail.ru>
2021-03-10 22:10:26 +0300

    min, max, avg - completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    let minVal = 1000;
+    for (let i = 0; i < array.length; i++) {
+        if (minVal > array[i]) {
+            minVal = array[i];
+        }
+    }
+    return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxVal = -1000;
+    for (let i = 0; i < array.length; i++) {
+        if (maxVal < array[i]) {
+            maxVal = array[i];
+        }
+    }
+    return maxVal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let count = 0;
+    for (let i = 0; i < array.length; i++) {
+        count += array[i];
+    }
+    count /= array.length;
+    return count;
 }
